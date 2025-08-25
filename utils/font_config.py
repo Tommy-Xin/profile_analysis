95140d98dc13ff1256bd9c8081b7169146ff539f
Tommy-Xin <xinhku168@gmail.com>
2025-08-25 14:22:18 +0800

    font change

--- utils/font_config.py
+++ utils/font_config.py
@@ -6,9 +6,9 @@ def set_chinese_font():
     system = platform.system()
     font_paths = []
     if system == "Windows":
-        font_paths = [r"C:\Windows\Fonts\msyh.ttc", r"C:\Windows\Fonts\simhei.ttf"]
+        font_paths = [r"C:\Windows\\Fonts\\msyh.ttc", r"C:\Windows\\Fonts\simhei.ttf"]
     elif system == "Darwin":  # macOS
-        font_paths = ["/System/Library/Fonts/PingFang.ttc"]
+        font_paths = ["/System/Library/Fonts/STHeiti Light.ttc"]
     else:  # Linux
         font_paths = ["/usr/share/fonts/truetype/wqy/wqy-zenhei.ttc"]
 
